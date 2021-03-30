19c043936bdcfed108a729307979a55ebc82fa7d
vagrius <vagrius1@bk.ru>
2021-03-30 11:22:11 +0400

    little changes

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .login_page import LoginPage
 
 
 class MainPage(BasePage):
@@ -11,3 +10,5 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
