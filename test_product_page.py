0d2f247f3351b7129a3aab458188b5d637199c74
vagrius <vagrius1@bk.ru>
2021-03-31 18:29:11 +0400

    united tests in class and added there setup fixture

--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,37 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
+import random
+import string
 
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
+class TestUserAddToBasketFromProductPage:
 
-@pytest.mark.parametrize('link', urls)
-def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    page.product_name_in_add_message_is_correct()
-    page.product_price_in_basket_is_correct()
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        letters = string.ascii_lowercase
+        email = ''.join(random.choice(letters) for _ in range(7)) + "@fakemail.org"
+        password = ''.join(random.choice(letters) for _ in range(10))
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.product_name_in_add_message_is_correct()
+        page.product_price_in_basket_is_correct()
 
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -21,13 +40,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_not_be_success_message()
-
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
     page.should_not_be_success_message()
 
 
