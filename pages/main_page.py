7a276c50ae113cd5bad6414788f94853448d7013
vagrius <vagrius1@bk.ru>
2021-04-02 09:54:20 +0400

    added comment

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,6 @@ from .base_page import BasePage
 
 
 class MainPage(BasePage):
-    pass
+    pass  # преподы в комментариях написали, что так тоже можно, но на всякий случай оставляю и первый вариант...
     # def __init__(self, *args, **kwargs):
     #     super(MainPage, self).__init__(*args, **kwargs)
