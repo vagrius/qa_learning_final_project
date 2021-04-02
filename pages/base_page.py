7c668d04257ded8a5f74b98adc16a2fcc3e4f375
vagrius <vagrius1@bk.ru>
2021-04-02 09:55:15 +0400

    arranged functions in alphabetical order of its names

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,9 +15,6 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
-
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
@@ -25,13 +22,6 @@ class BasePage:
     def go_to_basket_page(self):
         link = self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
         link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
-
-    def should_be_authorized_user(self):
-        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
-                                                                     " probably unauthorised user"
 
     def is_element_present(self, how, what):
         try:
@@ -54,6 +44,16 @@ class BasePage:
         except TimeoutException:
             return False
         return True
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
